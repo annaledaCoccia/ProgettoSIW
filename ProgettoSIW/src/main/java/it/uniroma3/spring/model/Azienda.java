b6d9884c5979ff6468f91f0eff3935a3fa11ac29
Albarganese <39588750+Albarganese@users.noreply.github.com>
2018-06-08 09:09:04 +0200

    Update Azienda.java

--- ProgettoSIW/src/main/java/it/uniroma3/spring/model/Azienda.java
+++ ProgettoSIW/src/main/java/it/uniroma3/spring/model/Azienda.java
@@ -1,5 +1,22 @@
 package it.uniroma3.spring.model;
 
-public class Azienda {
+public class Azienda
+{
+	
+	
+	private static Azienda single_instance = null;
+	
+	private Azienda()
+	{
+		
+	}
+	
+	public static Azienda getInstance()
+	{
+		if(single_instance == null)
+			single_instance = new Azienda();
+		
+		return single_instance;
+	}
 
 }
