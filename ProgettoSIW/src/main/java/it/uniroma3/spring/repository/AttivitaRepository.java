8bdc792d3861a5194543d017b700a8c8079c36f4
Albarganese <39588750+Albarganese@users.noreply.github.com>
2018-06-10 14:36:18 +0200

    Add files via upload

--- ProgettoSIW/src/main/java/it/uniroma3/spring/repository/AttivitaRepository.java
+++ ProgettoSIW/src/main/java/it/uniroma3/spring/repository/AttivitaRepository.java
@@ -1,14 +1,18 @@
 package it.uniroma3.spring.repository;
 
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-
 import it.uniroma3.spring.model.Attivita;
 
 public interface AttivitaRepository extends CrudRepository<Attivita, Long>
+
 {
+	
+	public Optional<Attivita> findByCodice(Long codice);
+	
 	public List<Attivita> findByNomeAttivita(String nomeAttivita);
 	
 	public List<Attivita> findByData(Date dataAttivita);
