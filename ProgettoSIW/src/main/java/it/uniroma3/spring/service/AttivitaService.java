e69667568ef7846783d6eb46f0933d9d6c24302e
Albarganese <39588750+Albarganese@users.noreply.github.com>
2018-06-10 14:39:21 +0200

    Update AttivitaService.java

--- ProgettoSIW/src/main/java/it/uniroma3/spring/service/AttivitaService.java
+++ ProgettoSIW/src/main/java/it/uniroma3/spring/service/AttivitaService.java
@@ -1,5 +1,46 @@
 package it.uniroma3.spring.service;
 
-public class AttivitaService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.spring.model.Attivita;
+import it.uniroma3.spring.repository.AttivitaRepository;
+
+@Transactional
+@Service
+public class AttivitaService {
+	
+	@Autowired
+	    private AttivitaRepository attivitaRepository; 
+        
+	   public List<Attivita> findAll() {
+	       return (List<Attivita>)this.attivitaRepository.findAll();
+	    }
+	   
+	   public Attivita save(Attivita attivita) {
+			return this.attivitaRepository.save(attivita);
+		}
+	   
+		public List<Attivita> findByNomeAttivita(String nome) {
+			return this.attivitaRepository.findByNomeAttivita(nome);
+		}
+
+		public Attivita findByCodice(Long codice) {
+			Optional<Attivita> attivita = this.attivitaRepository.findByCodice(codice);
+			if (attivita.isPresent()) 
+				return attivita.get();
+			else
+				return null;
+		}
+		public boolean alreadyExists(Attivita attivita) {
+			List<Attivita> attivital = this.attivitaRepository.findByNomeAttivita(attivita.getNomeAttivita());
+			if (attivital.size() > 0)
+				return true;
+			else 
+				return false;
+		}
 }
